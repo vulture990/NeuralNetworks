59b20f02c8011e3c57cd29ea7b6d3c86d5e2c964
salbi <mohamedsali602@live.com>
2021-05-17 18:09:54 +0100

    define the layer class

---
+++ layer.py
@@ -0,0 +1,10 @@
+class Layer:
+    def __init(self):
+        self.input=None
+        self.output=None
+    def forward(self,input):
+        #to return the output layer
+        pass
+    def backward(self,output_gradient,learning_rate):
+        #change para and return input derivative
+        pass
