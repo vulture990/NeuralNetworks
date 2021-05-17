774e25eac88895bb0b3c0e16549c0266efcab956
salbi <mohamedsali602@live.com>
2021-05-17 20:35:39 +0100

    almost

---
+++ Loss.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+
+def mse(y_true, y_pred):
+    return np.mean(np.power(y_true - y_pred, 2))
+
+
+def mse_prime(y_true, y_pred):
+    return 2 * (y_pred - y_true) / np.size(y_true)
